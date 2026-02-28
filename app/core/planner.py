7e9e991164c2fcc23c6e1f54724daa47ed1f0ae1
Siddharth <siddharth.shukla@adypu.edu.in>
2026-03-01 01:36:23 +0530

    adding more options in planner

--- app/core/planner.py
+++ app/core/planner.py
@@ -1,8 +1,28 @@
 from app.services.google_service import get_routes
 from app.traffic.optimizer import choose_best_route
-def plan_route(origin, destination):
-    routes = get_routes(origin, destination)
-    if not routes:
+from app.parking.parking_data import generate_parking_spots
+from app.parking.parking_optimizer import choose_best_parking
+def plan_route(origin, destination, use_parking=False):
+    if not use_parking:
+        routes = get_routes(origin, destination)
+        if not routes:
+            return None
+        best_route = choose_best_route(routes)
+        best_route["origin_coords"] = origin
+        return best_route
+    parking_spots = generate_parking_spots(destination)
+    best_parking_option = choose_best_parking(origin, destination, parking_spots)
+    if not best_parking_option:
         return None
-    best_route = choose_best_route(routes)
-    return best_route
+    best_parking_option["route"]["origin_coords"] = origin
+
+    return {
+        "mode": "parking",
+        "parking_id": best_parking_option["parking_id"],
+        "route": best_parking_option["route"],
+        "drive_time": best_parking_option["drive_time"],
+        "walking_time": best_parking_option["walking_time"],
+        "cost": best_parking_option["cost"],
+        "available_spots": best_parking_option["available_spots"],
+        "total_score": best_parking_option["total_score"],
+    }
