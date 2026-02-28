a4552554561e4ef59558e1e64af3e0d559042e41
Siddharth <siddharth.shukla@adypu.edu.in>
2026-02-28 19:57:01 +0530

    Imporving my rerouting

--- app/traffic/rerouter.py
+++ app/traffic/rerouter.py
@@ -1,16 +1,19 @@
 from app.services.google_service import get_routes
 from app.traffic.optimizer import choose_best_route
-def should_reroute(current_route, new_best_route, threshold=0.08):
-    current_remaining = current_route["traffic_duration"]
-    new_time = new_best_route["traffic_duration"]
-    improvement = (current_remaining - new_time) / current_remaining
-    return improvement > threshold
-
-def intelligent_reroute(current_position, destination, current_route):
+def intelligent_reroute(simulator, destination, threshold=0.08):
+    current_position = simulator.get_current_position()
     routes = get_routes(current_position, destination)
     if not routes:
-        return current_route, False
-    best_route = choose_best_route(routes)
-    if should_reroute(current_route, best_route):
-        return best_route, True
-    return current_route, False
+        return simulator.route, False
+    best_new_route = choose_best_route(routes)
+    total_points = len(simulator.points)
+    remaining_points = total_points - simulator.current_index
+    remaining_fraction = remaining_points / total_points
+    current_remaining_time = (
+        simulator.route["traffic_duration"] * remaining_fraction)
+    new_route_time = best_new_route["traffic_duration"]
+    improvement = (
+        current_remaining_time - new_route_time) / current_remaining_time
+    if improvement > threshold:
+        return best_new_route, True
+    return simulator.route, False
